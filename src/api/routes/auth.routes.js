da9d588d5d62ad39650295959314461760ba519a
IvanGMtz <navi.garces24@gmail.com>
2023-09-25 16:51:28 -0500

    feat: :sparkles: Update endpoint auth
    Add logout

--- src/api/routes/auth.routes.js
+++ src/api/routes/auth.routes.js
@@ -1,11 +1,13 @@
 import {Router} from 'express';
 import {postUserV1} from '../controllers/v1/user.js';
-import {login, register} from '../controllers/v1/auth.controller.js';
+import {login, register, logout} from '../controllers/v1/auth.controller.js';
 
 const appUser = Router();
 
 appUser.post('/register', register);
 
 appUser.post('/login', login);
+
+appUser.post('/logout', logout);
 
 export default appUser;
