616e312f3ed2edabcf778c67480f1b9910460f15
IvanGMtz <navi.garces24@gmail.com>
2023-09-27 17:06:40 -0500

    feat: :sparkles: Update request add login request

--- src/request/auth.js
+++ src/request/auth.js
@@ -4,3 +4,5 @@ const config = import.meta.env
 const API = `http://${config.VITE_HOST}:${config.VITE_PORT_BACKEND}`;
 
 export const registerRequest = user => axios.post(`${API}/auth/register`, user)
+
+export const loginRequest = user => axios.post(`${API}/auth/login`, user)
