a0fb2c7248fc2ce3fdd4eb85b09241feabe103a4
IvanGMtz <navi.garces24@gmail.com>
2023-09-27 10:03:30 -0500

    feat: :sparkles: Update Register page

--- src/components/pages/RegisterPage.jsx
+++ src/components/pages/RegisterPage.jsx
@@ -1,18 +1,33 @@
 import { useForm } from "react-hook-form";
+import { useAuth } from "../shared/AuthContext";
 
 function RegisterPage() {
-  
-    const { register } = useForm();
+
+  const { register, handleSubmit } = useForm();
+  const {signup, user} = useAuth();
+
+  console.log(user);
+
+  const onSubmit = handleSubmit(async (values) => {
+    signup(values)
+  })
 
   return (
-    <div>
-      <form>
-        <input type="text" name="username" />
-        <input type="email" name="email" />
-        <input type="password" name="password" />
+    <div className="bg-zinc-800 max-w-md p-10 rounded-md">
+      <form onSubmit={onSubmit}>
+        <input type="text" {...register('name', { required: true })} placeholder="Username"
+          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2" />
+        <input type="email" {...register('email', { required: true })} placeholder="Email"
+          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2" />
+        <input type="password" {...register('password', { required: true })} placeholder="Password"
+          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2" />
+        <button>
+          Register
+        </button>
+
       </form>
     </div>
   );
 }
 
-export default RegisterPage
+export default RegisterPage;
