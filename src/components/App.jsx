7d4cf87fc628789dc11e6dd6445db44b1246b4f2
IvanGMtz <navi.garces24@gmail.com>
2023-10-02 16:12:29 -0500

    feat: :sparkles: Use taskProvider and Navbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,27 +8,31 @@ import TaskPage from "./pages/TaskPage";
 import TaskFormPage from "./pages/TaskFormPage";
 import ProfilePage from "./pages/ProfilePage";
 import ProtectedRouted from "./shared/ProtectedRouted";
+import { TaskProvider } from "./shared/TaskContext";
+import Navbar from "./shared/Navbar";
 
 function App() {
 
   return (
     <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="*" element={<Error404 />} />
+      <TaskProvider>
+        <BrowserRouter>
+          <Navbar/>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="*" element={<Error404 />} />
 
-          <Route element={<ProtectedRouted />}>
-            <Route path="/tasks" element={<TaskPage />} />
-            <Route path="/add-task" element={<TaskFormPage />} />
-            <Route path="/tasks/:id" element={<TaskFormPage />} />
-            <Route path="/profile" element={<ProfilePage />} />
-          </Route>
-          
-        </Routes>
-      </BrowserRouter>
+            <Route element={<ProtectedRouted />}>
+              <Route path="/tasks" element={<TaskPage />} />
+              <Route path="/add-task" element={<TaskFormPage />} />
+              <Route path="/tasks/:id" element={<TaskFormPage />} />
+              <Route path="/profile" element={<ProfilePage />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </TaskProvider>
     </AuthProvider>
   );
 }
