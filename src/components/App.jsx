25dbc10338c8f098e2bcd976d95a0120182842c1
IvanGMtz <navi.garces24@gmail.com>
2023-10-02 13:31:45 -0500

    feat: :sparkles: Update app.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,10 @@ import LoginPage from "./pages/LoginPage";
 import { AuthProvider } from "./shared/AuthContext";
 import Error404 from "./pages/Error404";
 import HomePage from "./pages/HomePage";
+import TaskPage from "./pages/TaskPage";
+import TaskFormPage from "./pages/TaskFormPage";
+import ProfilePage from "./pages/ProfilePage";
+import ProtectedRouted from "./shared/ProtectedRouted";
 
 function App() {
 
@@ -14,11 +18,15 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
-          <Route path="/tasks" element={<h1>Task Page</h1>} />
-          <Route path="/add-task" element={<h1>new task</h1>} />
-          <Route path="/tasks/:id" element={<h1>update task</h1>} />
-          <Route path="/profile" element={<h1>profile</h1>} />
           <Route path="*" element={<Error404 />} />
+
+          <Route element={<ProtectedRouted />}>
+            <Route path="/tasks" element={<TaskPage />} />
+            <Route path="/add-task" element={<TaskFormPage />} />
+            <Route path="/tasks/:id" element={<TaskFormPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+          </Route>
+          
         </Routes>
       </BrowserRouter>
     </AuthProvider>
