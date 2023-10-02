3aaa1d2058d7ae9b911c3c14845fd29c07d0c8c2
IvanGMtz <navi.garces24@gmail.com>
2023-10-02 16:10:39 -0500

    feat: :sparkles: Update TaskFormPage

--- src/components/pages/TaskFormPage.jsx
+++ src/components/pages/TaskFormPage.jsx
@@ -1,7 +1,46 @@
+import { useForm } from "react-hook-form";
+import { useTasks } from "../shared/TaskContext";
+
 function TaskFormPage() {
+
+  const { register, handleSubmit } = useForm();
+
+  const { tasks, createTask } = useTasks();
+  console.log(tasks, createTask);
+  
+  const onSubmit = handleSubmit((data) => {
+    createTask(data);
+  })
+
   return (
-    <div>TaskFormPage</div>
-  )
+    <div className="bg-zinc-800 max-w-md w-full p-10 rounded-md">
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Title"
+          {...register("title")}
+          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+          autoFocus
+        />
+        <textarea
+          placeholder="Description"
+          rows="3"
+          {...register("description")}
+          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+        ></textarea>
+
+        <input
+          type="date"
+          {...register("date")}
+          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+        />
+
+        <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+          Save
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default TaskFormPage
