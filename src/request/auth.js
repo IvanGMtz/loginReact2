275859aa22dfd57058b98fc054d92b6200cc01e5
IvanGMtz <navi.garces24@gmail.com>
2023-10-02 11:56:35 -0500

    feat: :sparkles: Update url request

--- src/request/auth.js
+++ src/request/auth.js
@@ -1,8 +1,7 @@
-import axios from "axios";
-const config = import.meta.env
+import axios from "./axios";
 
-const API = `http://${config.VITE_HOST}:${config.VITE_PORT_BACKEND}`;
+export const registerRequest = user => axios.post(`/auth/register`, user)
 
-export const registerRequest = user => axios.post(`${API}/auth/register`, user)
+export const loginRequest = user => axios.post(`/auth/login`, user);
 
-export const loginRequest = user => axios.post(`${API}/auth/login`, user)
+export const verifyTokenRequest = () => axios.get('/auth/verify');
