3faa2f547f827b778cc5dad8c197df814077282d
IvanGMtz <navi.garces24@gmail.com>
2023-10-02 16:10:58 -0500

    feat: :sparkles: Update TaskPage

--- src/components/pages/TaskPage.jsx
+++ src/components/pages/TaskPage.jsx
@@ -1,9 +1,28 @@
+import { useEffect } from "react";
+import { useTasks } from "../shared/TaskContext"
+
 function TaskPage() {
 
-    
+  const { getTasks, tasks } = useTasks();
+
+  useEffect(() => {
+    getTasks()
+  },[]);
+
+  if(tasks.length === 0) return <h1>No tasks</h1>
 
   return (
-    <div>TaskPage</div>
+    <div>
+      {
+        tasks.map(task => (
+          <div key={task._id}>
+            <h1>{task.title}</h1>
+            <p>{task.description}</p>
+            <p>{task.date}</p>
+          </div>
+        ))
+      }
+    </div>
   )
 }
 
